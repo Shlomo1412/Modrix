1621217c863aa1c688b296326e59c50f0d3d0f08
Shlomo1412 <shlomofi050@gmail.com>
2025-06-04 18:50:15 +0300

    Fixed exception throws when trying to reopen a ProjectWorkspace. also allowing multiple ones.

--- Views/Pages/DashboardPage.xaml.cs
+++ Views/Pages/DashboardPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿
-using Modrix.ViewModels.Pages;
+﻿using Modrix.ViewModels.Pages;
 using Wpf.Ui.Abstractions.Controls;
 using System.Windows;
 using Modrix.Models;
@@ -7,23 +6,27 @@ using Modrix.Views.Controls;
 using Modrix.Views.Windows;
 using System;
 using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Wpf.Ui;
+using Wpf.Ui.Abstractions;
+using Modrix.ViewModels.Windows;
 
 namespace Modrix.Views.Pages
 {
     public partial class DashboardPage : INavigableView<DashboardViewModel>
     {
+        private readonly IServiceProvider _serviceProvider;
         public DashboardViewModel ViewModel { get; }
 
-        public DashboardPage(DashboardViewModel viewModel)
+        public DashboardPage(DashboardViewModel viewModel, IServiceProvider serviceProvider)
         {
             ViewModel = viewModel;
+            _serviceProvider = serviceProvider;
             DataContext = this;
 
             InitializeComponent();
             ViewModel.RefreshProjectsCommand.Execute(null);
-
         }
-
 
         private async void ProjectCard_DeleteClicked(object sender, RoutedEventArgs e)
         {
@@ -52,16 +55,34 @@ namespace Modrix.Views.Pages
         {
             if (sender is ProjectCard card && card.ProjectData is ModProjectData project)
             {
-                var workspaceWindow = App.Services.GetService<ProjectWorkspace>();
-                if (workspaceWindow != null)
-                {
-                    // 1) Load the project into the workspace VM
-                    workspaceWindow.LoadProject(project);
+                // Try to find an existing window for this project
+                var existingWindow = Application.Current.Windows.OfType<ProjectWorkspace>()
+                    .FirstOrDefault(w => w.ViewModel.CurrentProject?.Location == project.Location);
 
-                    // 2) Show (or Activate) the workspace window
-                    workspaceWindow.Show();
-                    workspaceWindow.Activate();
+                if (existingWindow != null)
+                {
+                    existingWindow.Activate();
+                    return;
                 }
+
+                // Get required services for ProjectWorkspace
+                var viewModel = _serviceProvider.GetRequiredService<ProjectWorkspaceViewModel>();
+                var navigationViewPageProvider = _serviceProvider.GetRequiredService<INavigationViewPageProvider>();
+                var navigationService = _serviceProvider.GetRequiredService<INavigationService>();
+
+                // Create a new workspace window with fresh dependencies
+                var workspaceWindow = new ProjectWorkspace(
+                    viewModel,
+                    navigationViewPageProvider,
+                    navigationService
+                );
+
+                // Load the project into the workspace VM
+                workspaceWindow.LoadProject(project);
+
+                // Show the new workspace window
+                workspaceWindow.Show();
+                workspaceWindow.Activate();
 
                 ViewModel.RefreshProjectsCommand.Execute(null);
             }
@@ -74,8 +95,5 @@ namespace Modrix.Views.Pages
                 ViewModel.OpenProjectFolderCommand.Execute(project);
             }
         }
-
-
-
     }
 }
