cd05dd2835694049b624304099bd15e4339cf0c7
Shlomo1412 <shlomofi050@gmail.com>
2025-06-18 15:51:39 +0300

    Enhance WentWrongDialog with scrolling and copy feature
    Updated WentWrongDialog.xaml to replace the error message display with a ScrollViewer for better handling of long messages. Added "Copy error" and "Close" buttons for improved user interaction.
    
    In WentWrongDialog.xaml.cs, introduced a private variable to store the error message and implemented functionality to copy the message to the clipboard.

--- Views/Windows/WentWrongDialog.xaml.cs
+++ Views/Windows/WentWrongDialog.xaml.cs
@@ -1,19 +1,26 @@
-using System.Windows;
 using Wpf.Ui.Controls;
 
 namespace Modrix.Views.Windows
 {
     public partial class WentWrongDialog : FluentWindow
     {
+        private string _errorMessage;
+
         public WentWrongDialog(string errorMessage)
         {
             InitializeComponent();
+            _errorMessage = errorMessage;
             ErrorMessageText.Text = errorMessage;
         }
 
         private void Close_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void CopyError_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(_errorMessage);
         }
     }
 }
