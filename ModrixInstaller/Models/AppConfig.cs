5c2d5aa40a19dbc2a36c766f765e183fd9b24598
Shlomo1412 <shlomofi050@gmail.com>
2025-09-22 15:32:37 +0300

    Added ModrixInstaller

---
+++ ModrixInstaller/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace ModrixInstaller.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
