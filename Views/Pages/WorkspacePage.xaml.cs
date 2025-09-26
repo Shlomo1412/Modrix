6723b2971770b3ba0713b6effb8ff6c97628f24b
Shlomo1412 <shlomofi050@gmail.com>
2025-09-26 15:28:56 +0300

    Cleanup unused using directives in WorkspacePage.xaml.cs
    Removed unnecessary `using` directives to streamline code.
    Combined relevant directives for better organization while retaining essential WPF references.

--- Views/Pages/WorkspacePage.xaml.cs
+++ Views/Pages/WorkspacePage.xaml.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Input;
